e5fb6b44d143042a38dc7e210142209cf98fe51f
Achille MARTIN <achart@noc.ac.uk>
2025-10-04 17:55:45 +0100

    Added leader key maps and warnings to the configuration

--- lua/nvim-robotics/init.lua
+++ lua/nvim-robotics/init.lua
@@ -1,2 +1,4 @@
+-- # Ensure that key mappings are loaded first
+-- # so that they are applied to the rest of the configuration
 require("nvim-robotics.keymaps")
 require("nvim-robotics.options")
---
+++ lua/nvim-robotics/keymaps.lua
@@ -0,0 +1,18 @@
+-- ----------------
+-- | KEY MAPPINGS |
+-- ----------------
+
+-- # Define <space> as leader key
+-- # for easiness of access on various keyboards
+-- # and minimal loss of standard functionality
+-- # Note: the leader key is a special key
+-- # used as a prefix to create custom shortcuts
+-- # WARNING: ensure that the leader key is defined
+-- # as the first key map so that it can be applied
+-- # to all subsequent mappings
+vim.g.mapleader = " "
+-- # Map local leader (used for local buffer)
+-- # to the same key as general leader
+-- # for consistency
+vim.g.maplocalleader = " "
+
