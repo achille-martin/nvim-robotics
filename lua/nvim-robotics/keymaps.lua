5b559d57df4ec262a7680e76900f4245726789a0
Achille MARTIN <achart@noc.ac.uk>
2025-10-07 02:30:32 +0100

    Added a special mode Ctrl-space to enable quick shortcuts in all modes

--- lua/nvim-robotics/keymaps.lua
+++ lua/nvim-robotics/keymaps.lua
@@ -31,12 +31,27 @@ vim.api.nvim_set_keymap(
     '<Esc>',
     { noremap=true, silent=true }
 )
+-- # Ensure access to Visual Block with `Ctrl + q`
+-- # because most terminals will paste clipboard with `Ctrl + v`
 
--- # Reload current file with `<leader>r`
+-- # Define a special mode
+-- # to input extra keys in any mode, easily
+-- # This mode is `<Ctrl-space>`
+-- # Add your key after the Ctrl sequence to access a functionality
+-- # TODO: bring up a little window with key options
+-- # to select functionality when special mode is entered
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>r",
-    ':so %<CR>:echo"[INFO] Reloaded current file"<CR>',
+    "<C-space>",
+    ':echo"[WARN] Tried to activate special mode but requires key input"<CR>',
+    { noremap=true, silent=true }
+)
+
+-- # Reload current file with `<Ctrl-space>r`
+vim.api.nvim_set_keymap(
+    "n",
+    "<C-space>r",
+    ':so %<CR>:echo"[SPECIAL] Reloaded current file"<CR>',
     { noremap=true, silent=true }
 )
 
@@ -45,111 +60,169 @@ vim.api.nvim_set_keymap(
 -- # because it is used as `Esc`
 -- # Note continued: it is also not possible to map `Ctrl + Shift + c`
 -- # because currently detected as `Ctrl + c`
+-- # Note further: it is also not practical to only map `<leader>c`
+-- # because it might not be practical in all modes
+-- #
+-- # OPTION 1: for new Neovim users
+-- # use `yy` (whole line), `y` (selection), or right click to copy
+-- #
+-- # OPTION 2: for advanced Neovim users
 -- # In NORMAL mode, copy current line into the "a" register
--- # with `<leader>c`
+-- # with `<Ctrl-space>c`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>c",
-    '"ayy:echo"[INFO] Copied current line into the register a"<CR>',
+    "<C-space>c",
+    '"ayy:echo"[SPECIAL] Copied current line into the register a"<CR>',
     { noremap=true, silent=false }
 )
 -- # In VISUAL mode, copy selected text into the "a" register
--- # with `<leader>c`
+-- # with `<Ctrl-space>c`
 vim.api.nvim_set_keymap(
     "v",
-    "<leader>c",
-    '"ay:echo"[INFO] Selected text copied into the register a"<CR>',
+    "<C-space>c",
+    '"ay:echo"[SPECIAL] Selected text copied into the register a"<CR>',
     { noremap=true, silent=false }
 )
 -- # In NORMAL mode, share the "a" register
--- # with the unnamed and plus registers
+-- # with the plus register
+-- # using `<Ctrl-space>a`
+-- # Note: this is used to export the internal register to the clipboard
+-- # Think a like a-out
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>a",
-    ':let @+=@a<CR>:echo"Populated register + from register a"<CR>',
+    "<C-space>a",
+    ':let @+=@a<CR>:echo"[SPECIAL] Populated register + from register a"<CR>',
+    { noremap=true, silent=true }
+)
+-- # In NORMAL mode, share the plus register
+-- # with the "a" register
+-- # using `<Ctrl-space>z`
+-- # Note: this is used to export the clipboard to the internal register
+-- # Think z like z-back-in
+vim.api.nvim_set_keymap(
+    "n",
+    "<C-space>z",
+    ':let @a=@+<CR>:echo"[SPECIAL] Populated register a from register +"<CR>',
     { noremap=true, silent=true }
 )
 -- # Define cut shortcuts
+-- #
+-- # OPTION 1: for new Neovim users
+-- # use `dd` (whole line) or `d` / `x`, or right click to cut
+-- #
+-- # OPTION 2: for advanced Neovim users
 -- # In NORMAL mode, cut current line
 -- # and save the content into the "a" register
--- # with `<leader>x`
+-- # with `<Ctrl-space>x`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>x",
-    '"add:echo"Current line cut and saved into register a"<CR>',
+    "<C-space>x",
+    '"add:echo"[SPECIAL] Current line cut and saved into register a"<CR>',
     { noremap=true, silent=false }
 )
 -- # In VISUAL mode, cut selected text
 -- # and save the content into the "a" register
--- # with `<leader>x`
+-- # with `<Ctrl-space>x`
 vim.api.nvim_set_keymap(
     "v",
-    "<leader>x",
-    '"ad:echo"Selected line cut and saved into register a"<CR>',
+    "<C-space>x",
+    '"ad:echo"[SPECIAL] Selected line cut and saved into register a"<CR>',
     { noremap=true, silent=false }
 )
 -- # Define paste shortcuts
--- # In NORMAL mode, paste the "a" register one line below with `<leader>v`
+-- #
+-- # OPTION 1: for new Neovim users
+-- # use `p` or right click to paste
+-- #
+-- # OPTION 2: for advanced Neovim users
+-- # In NORMAL mode, paste the "a" register one line below
+-- # with `<Ctrl-space>v`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>v",
-    ':pu a<CR>:echo"Pasted saved line from register a"<CR>',
-    { noremap=true, silent=false }
-)
--- # In INSERT mode, use `Ctrl + v` or `Ctrl + Shift + v`
--- # to paste the "+" register?
--- # because this is a terminal-linked command
--- # How about the "a" register?
--- # Note: leader key cannot be used in INSERT mode if it is <space>
--- # Compromise is to use `Alt/AltGr + v` to paste
--- # from the "a" register right on cursor in INSERT mode?
--- # TODO: find a more consistent pasting while in INSERT mode
--- # to try to re-use the leader key
-vim.api.nvim_set_keymap(
-    "i",
-    "<M-v>",
-    '<C-r>a',
+    "<C-space>v",
+    ':pu a<CR>:echo"[SPECIAL] Pasted saved line from register a"<CR>',
     { noremap=true, silent=false }
 )
 -- # In VISUAL mode, paste the "a" register at the selection
 -- # while replacing the selection
--- # with `<leader>v`
+-- # with `<Ctrl-space>v`
 vim.api.nvim_set_keymap(
     "v",
-    "<leader>v",
-    'di<C-r>a<Esc>',
+    "<C-space>v",
+    'di<C-r>a<Esc>:echo"[SPECIAL] Pasted saved line from register a"<CR>',
+    { noremap=true, silent=false }
+)
+-- # In INSERT mode, paste the "a" register right on the cursor
+-- # with `<Ctrl-space>v`
+-- # Note: multi-line blocks might appear disformed
+vim.api.nvim_set_keymap(
+    "i",
+    "<C-space>v",
+    '<C-r>a',
+    { noremap=true, silent=false }
+)
+-- # In COMMAND mode, paste the "a" register right on the cursor
+-- # with `<Ctrl-space>v`
+vim.api.nvim_set_keymap(
+    "c",
+    "<C-space>v",
+    '<C-r>a',
+    { noremap=true, silent=false }
+)
+-- # In TERMINAL mode, paste the "a" register right on the cursor
+-- # with `<Ctrl-space>v`
+vim.api.nvim_set_keymap(
+    "t",
+    "<C-space>v",
+    '<C-\\><C-n>"apa',
     { noremap=true, silent=false }
 )
 
 -- # Define undo/redo shortcuts
--- # In NORMAL mode, undo last action with `<leader><backspace>`
+-- #
+-- # OPTION 1: for new Neovim users
+-- # use `u` (undo) or `Ctrl + r` (redo)
+-- #
+-- # OPTION 2: for advanced Neovim users
+-- # In NORMAL mode, undo last action with `<Ctrl-space><backspace>`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader><BS>",
+    "<C-space><BS>",
     'u',
     { noremap=true, silent=false }
 )
+-- # In NORMAL mode, redo last action with `<Ctrl-space><enter>`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader><CR>",
+    "<C-space><CR>",
     '<C-r>',
     { noremap=true, silent=false }
 )
 
 -- # Define save shortcuts
--- # In NORMAL mode, write current buffer with `<leader>s`
+-- #
+-- # OPTION 1: for new Neovim users
+-- # use `:w`
+-- #
+-- # OPTION 2: for advanced Neovim users
+-- # In NORMAL mode, write current buffer with `<Ctrl-space>s`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>s",
+    "<C-space>s",
     ':w<CR>',
     { noremap=true, silent=false }
 )
 
 -- # Define exit shortcuts
--- # In NORMAL mode, exit without saving with `<leader>q`
+-- #
+-- # OPTION 1: for new Neovim users
+-- # use `:q`
+-- #
+-- # OPTION 2: for advanced Neovim users
+-- # In NORMAL mode, exit without saving with `<Ctrl-space>q`
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>q",
+    "<C-space>q",
     ':q<CR>',
     { noremap=true, silent=false }
 )
