05c73d97ad408b583eba2e48d832a6eb8c1fd1ae
Achille MARTIN <achart@noc.ac.uk>
2025-10-27 01:32:36 +0000

    Updated comment symbols in init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
--- Get the custom nvim config name
--- from the parent folder of this init script
--- and make it a global variable
--- for use in lower levels of config
+-- # Get the custom nvim config name
+-- # from the parent folder of this init script
+-- # and make it a global variable
+-- # for use in lower levels of config
 vim.g.custom_nvim_config_name = vim.fn.expand('<sfile>:h:t')
 
--- Load the base nvim-robotics config
--- whose name can be different from the custom nvim config name
+-- # Load the base nvim-robotics config
+-- # whose name can be different from the custom nvim config name
 require("nvim-robotics")
